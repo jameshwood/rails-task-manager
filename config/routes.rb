a779bd077c1923670a2031146e752fb485209405
James Wood <jameshwood94@gmail.com>
2024-11-26 14:08:03 +0000

    complete with no styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
   # root "posts#index"
   get '/tasks', to: "tasks#index"
   get 'tasks/new', to: "tasks#new"
-  post '/task', to: 'tasks#create', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
 
-  get '/:id', to: "tasks#show", as: 'task'
+  get 'tasks/:id', to: "tasks#show", as: 'task'
+  get 'tasks/:id/edit', to: "tasks#edit", as: 'edit_task'
+  patch 'tasks/:id', to: "tasks#update"
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
 
 end
